b3b5aedf3729b37b1bfb1be0d0920422615d7dcd
EricCasey <EricCasey509@gmail.com>
2016-12-19 21:18:20 -0500

    webpack config

--- server.js
+++ server.js
@@ -10,16 +10,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-const COLUMNS = [
-  'carbohydrate_g',
-  'protein_g',
-  'fa_sat_g',
-  'fa_mono_g',
-  'fa_poly_g',
-  'kcal',
-  'description',
-];
-
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
