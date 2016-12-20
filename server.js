b5fe74405a5dee76f0fc2f7aed4cb6de9098335c
EricCasey <EricCasey509@gmail.com>
2016-12-19 21:26:53 -0500

    adding https mauybe

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+  res.send('i7rChiYfAo6xKF8Jx6XP8157p4znpITI-fsoeCpQMkE.fEFd7HcqekFNdT_M3-QYnFGRdl6nRWjwIqAPppaIfUs')
+})
+
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
