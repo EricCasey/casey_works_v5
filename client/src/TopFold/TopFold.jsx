56af94707ba86db19b84ab125391094121d5b22f
EricCasey <EricCasey509@gmail.com>
2017-01-02 20:24:18 -0500

    asd

--- client/src/TopFold/TopFold.jsx
+++ client/src/TopFold/TopFold.jsx
@@ -7,9 +7,9 @@ class TopFold extends Component {
       <div className="TopFold" id="TopFold">
       <div className="anchor1"></div>
         <div className="content">
-          <h3>Hi there, welcome to my 'brutalist' website.</h3>
-          <h3>Below you'll find most of my Web Development projects & contact info.</h3>
-          <h3>As of December 2016 this site is a work in progress/sandbox.</h3>
+          <h3>Hi there, welcome to my website.</h3>
+          <h3>On mobile, this URL directs you to my <a href="http://resume.casey.works">resmue</a>.</h3>
+          <h3>As of January 2017 this site is a work in progress/sandbox.</h3>
           <h3>Thanks for visting.</h3>
           <h3>-Eric</h3>
         </div>
