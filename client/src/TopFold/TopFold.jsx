0c317e945dc70dc2dea7c99b372da51ab84c86c2
Eric Casey <ericcasey509@gmail.com>
2018-06-16 13:35:26 -0400

    asd

--- client/src/TopFold/TopFold.jsx
+++ client/src/TopFold/TopFold.jsx
@@ -5,13 +5,13 @@ class TopFold extends Component {
   render() {
     return (
       <div className="TopFold" id="TopFold">
-      <div className="anchor1"></div>
+        <div className="anchor1"></div>
         <div className="content">
           <h3>Hi there, welcome to my website.</h3>
           <h3>On mobile, this URL directs you to my <a href="http://resume.casey.works">CV</a>.</h3>
           <h3>Heroku-Node-ReactJS-based site - WIP est. Nov 2016</h3>
           <h3>Thanks for the session. No Bounce SVP.</h3>
-          <h3>Eric || u/Casey_Works</h3>
+          <h3>Eric IRL || /u/Casey_Works || @The_Casey_Works</h3>
         </div>
       </div>
     );
